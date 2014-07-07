6fbb303e5e1f59660f7b269352f9ecdf2f54ad7b
Brandon Brown <brandon.brown@ymail.com>
2014-07-07 11:57:21 -0700

    deleteing some unneeded files

--- app/js/views/methodology-view.js
+++
@@ -1,8 +0,0 @@
-var Backbone = require('backbone')
-  , Marionette = require('backbone.marionette')
-  , App = require('../app')
-  , template = require('../templates/help-template.hbs');
-
-module.exports = Marionette.Layout.extend({
-	template: template
-});
