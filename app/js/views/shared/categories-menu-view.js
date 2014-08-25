2026a09dd87ec5268aab5cc0f3a480fe5b237710
Brandon Brown <brandon.brown@ymail.com>
2014-08-25 13:54:57 -0700

    added Maurice's animation code for the navigation menu icons

--- app/js/views/shared/categories-menu-view.js
+++ app/js/views/shared/categories-menu-view.js
@@ -6,5 +6,24 @@ module.exports = Marionette.CollectionView.extend({
   className: 'menu',
   onRender: function() {
     $(this.el).append('<li class="help"><a href="#" data-toggle="modal" data-target="#helpModal" class="btn btn-default btn-circle-36 ico-help">Help</a></li>');
+
+    $(function(){
+      var fast = 200;
+      var faster = 50;
+      $("#col-nav li").hover(function(){
+        $(this).stop().animate({width:'240px'},fast,function(){});
+        $(this).find('a').stop().animate({width:'240px'},fast);
+        $(this).find('.checkmark').stop()
+          .animate({top:'6px'},faster)
+          .animate({right:'6px'},faster);
+      }, function() {
+        $(this).stop().animate({width:'36px'},fast);
+        $(this).find('a').stop().animate({width:'36px'},fast);
+        $(this).find('.checkmark').stop()
+          .animate({top:'-8px'},faster)
+          .animate({right:'-8px'},faster);
+      });
+    });
+
   }
 });
