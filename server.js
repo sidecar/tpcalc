e91300cd22cbeae22bc4b8cea840651aab6326e3
Brandon Brown <brandon.brown@ymail.com>
2014-09-11 10:37:54 -0700

    tweaks to the gate form code

--- server.js
+++ server.js
@@ -115,30 +115,3 @@ app.get('/*', function(req, res){
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
