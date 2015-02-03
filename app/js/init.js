6f2cd180acebf90d7f4f50f7ae35467bb32fbb85
Brandon Brown <brandon.brown@ymail.com>
2015-02-03 12:12:38 -0800

    change to .gitignore

--- app/js/init.js
+++ app/js/init.js
@@ -1,7 +1,5 @@
 "use strict";
 var App = require('./app');
-// This entire file is here because for some fucking hard to understand reason you cannot start app.js 
-// and then export it to be referenced as a module at the end of app.js. 
-// It doesn't work you have to start it somewhere else. 
-// No clue why this is.
-App.start(); 
+// You cannot start app.js and then export it to be referenced as a module at the end of app.js.
+// You have to start it in a seperate file.
+App.start();
